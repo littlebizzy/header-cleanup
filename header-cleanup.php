b02fcd1c9d7fe7b83cd5b2705c8d7e1c5461bc04
Jesse Nickles <jesse@littlebizzy.com>
2018-08-25 02:36:03 -0500

    Update header-cleanup.php

--- header-cleanup.php
+++ header-cleanup.php
@@ -3,7 +3,7 @@
 Plugin Name: Header Cleanup
 Plugin URI: https://www.littlebizzy.com/plugins/header-cleanup
 Description: Cleans up most of the unnecessary junk meta included by default in the WordPress header including generator, RSD, shortlink, previous and next, etc.
-Version: 1.0.2
+Version: 1.1.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -17,7 +17,7 @@ namespace LittleBizzy\HeaderCleanup;
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = 'hdrcln';
-const VERSION = '1.0.2';
+const VERSION = '1.1.0';
 
 // Block direct calls
 if (!function_exists('add_action'))
